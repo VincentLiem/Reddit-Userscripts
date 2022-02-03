c319054d0d18fdb2c8248319845f520c7f92a141
petkeeper18 <96896686+petkeeper18@users.noreply.github.com>
2022-02-02 17:49:33 -0800

    Create Switch-to-Old-Reddit.js

---
+++ Switch-to-Old-Reddit.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Switch to Old Reddit
+// @version      1.0
+// @match        *.reddit.com/*
+// @match        https://*.reddit.com/*
+// @exclude      *://old.reddit.com/*
+// @exclude      *://www.reddit.com/gallery*
+// @exclude      *://www.reddit.com/poll*
+// @run-at       document-start
+
+// ==/UserScript==
+ 
+var address = window.location.href;
+address = address.replace("www.reddit", "old.reddit");
+
+
+window.location.replace(address);
