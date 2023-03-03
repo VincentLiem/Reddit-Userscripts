a1292b2a59faf12b203dc3a1b07d015f99de5339
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2023-03-02 21:23:20 -0800

    stop chat.reddit from freezing page

--- Switch-to-Old-Reddit.js
+++ Switch-to-Old-Reddit.js
@@ -5,8 +5,9 @@
 // @exclude      *://old.reddit.com/*
 // @exclude      *://www.reddit.com/gallery*
 // @exclude      *://www.reddit.com/poll*
+// @exclude      *chat.reddit.com/*
 // @run-at       document-start
 
 // ==/UserScript==
- 
+
 window.location.replace(window.location.href.replace("www.reddit", "old.reddit"));
